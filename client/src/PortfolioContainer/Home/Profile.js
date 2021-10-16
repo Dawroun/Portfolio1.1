2cfcdcfd07fbc4153abee054a6abb1b3c9f07861
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-16 23:11:05 +0500

    working on structure of profile page

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="#">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-google-plus-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-youtube-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
