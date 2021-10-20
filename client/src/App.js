91169d7bcadca8971bd1b3868a882a2ec3164068
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-20 18:43:56 +0500

    footer of profile page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Profile } from './PortfolioContainer/Home/Profile';
+import Footer from './PortfolioContainer/Home/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Footer />
     </div>
   );
 }
