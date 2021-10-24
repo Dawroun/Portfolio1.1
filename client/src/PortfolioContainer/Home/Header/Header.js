16dd96241d53d6a9ec5ec9ba719bc080def43ec0
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-24 22:08:59 +0500

    header responsiveness

--- client/src/PortfolioContainer/Home/Header/Header.js
+++ client/src/PortfolioContainer/Home/Header/Header.js
@@ -64,7 +64,7 @@ export default function Header() {
             <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
           </div>
           <div className="header-logo">
-            <span>Dawroun~Web</span>
+            <span>Dawroun~</span>
           </div>
           <div
             className={
