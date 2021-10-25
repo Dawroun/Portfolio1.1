b61e313d5500d0e3b5f2abab96c9bf78913ba613
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-25 18:41:53 +0500

    next commit

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animations from '../../utilities/Animations'
+
+export default function AboutMe() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- client/src/utilities/commonUtils.js
+++ client/src/utilities/commonUtils.js
@@ -1,17 +1,23 @@
 import Home from "../PortfolioContainer/Home/Home";
+import AboutMe from "../PortfolioContainer/AboutMe/AboutMe";
 
 export const TOTAL_SCREENS = [
     {
         screen_name: "Home",
         component: Home,
-    }
-]
+    },
+    {
+        screen_name: "AboutMe",
+        component: AboutMe,
+    },
+];
 
 export const GET_SCREEN_INDEX = (screen_name) => {
     if (!screen_name) return -1;
+
     for (let i = 0; i < TOTAL_SCREENS.length; i++) {
-        if (TOTAL_SCREENS[i] === screen_name) return i;
+        if (TOTAL_SCREENS[i].screen_name === screen_name) return i;
     }
 
-    return -1;  
-}
+    return -1;
+};
