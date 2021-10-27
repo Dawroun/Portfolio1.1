10bde22b7024243817a4458a1b61f22b34e25f28
Davron Abdukhakimov <davron2806@gmail.com>
2021-10-27 16:54:17 +0500

    modified AboutMe

--- client/src/PortfolioContainer/AboutMe/AboutMe.js
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -12,10 +12,46 @@ export default function AboutMe(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+    const SCREEN_CONSTANTS = {
+      description: "Full Stack Web and Mobile Developer",
+      highlights: {
+        bullets: [
+          "Full Stack Web and Mobile Development",
+          "Interactive Front End design",
+          "React and React Native",
+          "Redux for State Management",
+          "Building APIs"
+        ],
+        heading: "Here are a Few Highlights:"
+      }
+    }
+
+    const renderHighlight = () => {
+      return (
+        SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+          <div className="highlight" key ={i}>
+            <div className="highlight-blob"></div>
+            <span>{value}</span>
+          </div>
+        ))
+      )
+    }
+
   return (
     <div className="about-me-container screen-container" id={props.id || ""}>
       <div className="about-me-parent">
         <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">{SCREEN_CONSTANTS.description}</span>
+            <div className="about-me-highlights">
+              <div className="highlights-heading">
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
