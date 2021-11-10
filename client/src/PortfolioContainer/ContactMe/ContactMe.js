8b4b4993a74b12acb48222af60db1f711ac02dce
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-10 12:11:21 +0500

    nearly done with contact me component

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Typical from "react-typical";
 
 import imgBack from "../../../src/images/mailz.jpeg";
@@ -6,6 +6,8 @@ import load1 from "../../../src/images/load2.gif";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
+
+import "./ContactMe.css";
 
 export default function ContactMe(props) {
   let fadeInScreenHandler = (screen) => {
@@ -15,6 +17,24 @@ export default function ContactMe(props) {
 
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
+  console.log(name);
 
   return (
     <div className="main-container" id={props.id || ""}>
@@ -40,6 +60,29 @@ export default function ContactMe(props) {
           <a href="https://twitter.com/Davron87284843?s=09">
             <i className="fa fa-twitter"></i>
           </a>
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Email Here!</h4>
+            <img src={imgBack} alt="image not found" />
+          </div>
+          <form>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" onChange={handleEmail} value={email} />
+
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+
+            <div className="send-btn">
+              <button type="submit">
+                send <i className="fa fa-paper-plane" />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
