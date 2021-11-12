98f7e6c914e1023ccf706ef512a8a99c2dd5b22c
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-12 19:45:49 +0500

    trying to fix the backend bug

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,43 +1,48 @@
-const router = require('express').Router()
-const nodemailer = require('nodemailer')
+const router = require("express").Router();
+const nodemailer = require("nodemailer");
 
-router.post('/contact', (req, res) => {
-    let data = req.body
-    if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0) {
-        return res.json({ msg: "Please All The Fields" })
+router.post("/contact", (req, res) => {
+    let data = req.body;
+    if (
+        data.name.length === 0 ||
+        data.email.length === 0 ||
+        data.message.length === 0
+    ) {
+        return res.json({ msg: "Please Fill All The Fields!" });
     }
 
+    let smtpTransporter = nodemailer.createTransport({
+        service: "Gmail",
+        port: 465,
+        auth: {
+            user: "davron2806@gmail.com",
+            pass: "19982806",
+        },
+    });
+    let mailOptions = {
+        from: data.email,
+        to: "davron2806@gmail.com",
+        subject: `message from ${data.name}`,
+        html: `
 
-        let smtpTransporter = nodemailer.createTransport({
-            service: 'Gmail',
-            port: 465,
-            auth: {
-                user: 'davron2806gmail.com',
-                pass: '19982806'
-            }
-        })
-        let mailOptions = {
-            from: data.email,
-            to: 'davron2806gmail.com',
-            subject: `message from ${data.name}`,
-            html: `
-            <h3>Informations</h3>
+            <h3>Informations<h3/>
             <ul>
-            <li>Name : ${data.name}</li>
-            <li>Email : ${data.email}</li>
+            <li>Name: ${data.name}<li/>
+            <li>Email: ${data.email}<li/>
             </ul>
             <h3>Message</h3>
-            <p>${data.message}</p>
-            `
-        }
-        smtpTransporter.sendMail(mailOptions, (error) => {
-            try {
-                if (error) return res.status(400).json({ msg: "Please fill all the fields" })
-                res.status(200).json({ msg: "Thank you for contacting Davron" })
-            } catch (error) {
-                if (error) return res.status(500).json({ msg: "There is server error" })
-            }
-        })
-})
+            <p>${data.message}<p/>
+            `,
+    };
 
-module.exports = router
+    smtpTransporter.sendMail(mailOptions, (error) => {
+        try {
+            if (error)
+                return res.status(400).json({ msg: "Please Fill All The Fields!" });
+            res.status(200).json({ msg: "Thank You For Contacting Davron." });
+        } catch (error) {
+            if (error) return res.status(500).json({ msg: "There is server error" });
+        }
+    });
+});
+module.exports = router;
