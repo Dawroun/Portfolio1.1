e3a50e0f7ec61a230f30e10c97d09626fd017391
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-12 18:58:21 +0500

    creating server

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config;
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const app = express();
+
+//creating the middlware
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT;
+app.listen(port, console.log(`server listening to port 5000`))
