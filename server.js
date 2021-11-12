b1b110baad46f8c19c491029d843799c905ec183
Davron Abdukhakimov <davron2806@gmail.com>
2021-11-12 19:23:30 +0500

    Contact Route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config;
 const express = require("express")
 const cors = require("cors")
 const path = require("path")
+const contactRoute = require('./route/contactRoute')
 
 const app = express();
 
